2310233674251c4bf7311da957e3d15d74184679
Farouk Brown <aionoid.xd@gmail.com>
2025-05-12 21:02:21 +0100

    :alien: Add package and update python to 312

--- extensions/default.nix
+++ extensions/default.nix
@@ -1,48 +1,50 @@
-{ callPackage }:
-
-{
+{callPackage}: {
   acly-inpaint =
-    callPackage ./acly-inpaint { };
+    callPackage ./acly-inpaint {};
   acly-tooling =
-    callPackage ./acly-tooling { };
+    callPackage ./acly-tooling {};
   badcafecode-execution-inversion-demo =
-    callPackage ./badcafecode-execution-inversion-demo { };
+    callPackage ./badcafecode-execution-inversion-demo {};
   city96-gguf =
-    callPackage ./city96-gguf { };
+    callPackage ./city96-gguf {};
   cubiq-essentials =
-    callPackage ./cubiq-essentials { };
+    callPackage ./cubiq-essentials {};
   cubiq-instantid =
-    callPackage ./cubiq-instantid { };
+    callPackage ./cubiq-instantid {};
   cubiq-ipadapter-plus =
-    callPackage ./cubiq-ipadapter-plus { };
+    callPackage ./cubiq-ipadapter-plus {};
   ellangok-post-processing =
-    callPackage ./ellangok-post-processing { };
+    callPackage ./ellangok-post-processing {};
   extraltodeus-skimmed-cfg =
-    callPackage ./extraltodeus-skimmed-cfg { };
+    callPackage ./extraltodeus-skimmed-cfg {};
   fannovel16-controlnet-aux =
-    callPackage ./fannovel16-controlnet-aux { };
+    callPackage ./fannovel16-controlnet-aux {};
   fannovel16-frame-interpolation =
-    callPackage ./fannovel16-frame-interpolation { };
+    callPackage ./fannovel16-frame-interpolation {};
   fizzledorf-fizz =
-    callPackage ./fizzledorf-fizz { };
+    callPackage ./fizzledorf-fizz {};
   florestefano1975-portrait-master =
-    callPackage ./florestefano1975-portrait-master { };
+    callPackage ./florestefano1975-portrait-master {};
   gourieff-reactor =
-    callPackage ./gourieff-reactor { };
+    callPackage ./gourieff-reactor {};
   huchenlei-layerdiffuse =
-    callPackage ./huchenlei-layerdiffuse { };
+    callPackage ./huchenlei-layerdiffuse {};
   kijai-ic-light =
-    callPackage ./kijai-ic-light { };
+    callPackage ./kijai-ic-light {};
   kijai-supir =
-    callPackage ./kijai-supir { };
+    callPackage ./kijai-supir {};
   kosinkadink-advanced-controlnet =
-    callPackage ./kosinkadink-advanced-controlnet { };
+    callPackage ./kosinkadink-advanced-controlnet {};
   kosinkadink-animatediff-evolved =
-    callPackage ./kosinkadink-animatediff-evolved { };
+    callPackage ./kosinkadink-animatediff-evolved {};
   kosinkadink-video-helper-suite =
-    callPackage ./kosinkadink-video-helper-suite { };
+    callPackage ./kosinkadink-video-helper-suite {};
   lev145-images-grid =
-    callPackage ./lev145-images-grid { };
+    callPackage ./lev145-images-grid {};
   ssitu-ultimate-sd-upscale =
-    callPackage ./ssitu-ultimate-sd-upscale { };
+    callPackage ./ssitu-ultimate-sd-upscale {};
+  impact-pack =
+    callPackage ./impact-pack {};
+  impact-subpack =
+    callPackage ./impact-subpack {};
 }
